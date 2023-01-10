3c1310790968be4e19dc5b41742471a83b3dd228
yagirl-belle <zahlia.hamer@students.mq.edu.au>
2023-01-10 11:46:19 +1100

    adjusted editor

--- js/editor.js
+++ js/editor.js
@@ -1,5 +1,5 @@
 const blogTitleField = document.querySelector('.title');
-const articleField = document.querySelector('.article');
+const articleFeild = document.querySelector('.article');
 
 // banner
 const bannerImage = document.querySelector('#banner-upload');
@@ -9,8 +9,6 @@ let bannerPath;
 const publishBtn = document.querySelector('.publish-btn');
 const uploadInput = document.querySelector('#image-upload');
 
-
-/**Event Listeners for changes */
 bannerImage.addEventListener('change', () => {
     uploadImage(bannerImage, "banner");
 })
@@ -19,7 +17,6 @@ uploadInput.addEventListener('change', () => {
     uploadImage(uploadInput, "image");
 })
 
-/*Upload Function*/
 const uploadImage = (uploadFile, uploadType) => {
     const [file] = uploadFile.files;
     if(file && file.type.includes("image")){
@@ -43,17 +40,16 @@ const uploadImage = (uploadFile, uploadType) => {
     }
 }
 
-/*Inserts a text format of our image*/
 const addImage = (imagepath, alt) => {
-    let curPos = articleField.selectionStart;
+    let curPos = articleFeild.selectionStart;
     let textToInsert = `\r![${alt}](${imagepath})\r`;
-    articleField.value = articleField.value.slice(0, curPos) + textToInsert + articleField.value.slice(curPos);
+    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
 }
 
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 publishBtn.addEventListener('click', () => {
-    if(articleField.value.length && blogTitleField.value.length){
+    if(articleFeild.value.length && blogTitleField.value.length){
         // generating id
         let letters = 'abcdefghijklmnopqrstuvwxyz';
         let blogTitle = blogTitleField.value.split(" ").join("-");
@@ -66,10 +62,10 @@ publishBtn.addEventListener('click', () => {
         let docName = `${blogTitle}-${id}`;
         let date = new Date(); // for published at info
 
-        //access firestore with db variable;
+        //access firstore with db variable;
         db.collection("blogs").doc(docName).set({
             title: blogTitleField.value,
-            article: articleField.value,
+            article: articleFeild.value,
             bannerImage: bannerPath,
             publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
         })
