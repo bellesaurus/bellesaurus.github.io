efa4dd32bd7917fdf73e28ff40bb2ccb7256b5f5
yagirl_belle <zahlia.hamer@students.mq.edu.au>
2023-01-10 21:58:17 +1100

    updated to hide API key

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 let firebaseConfig = {
-    apiKey: "AIzaSyCBIdeWRFq_UYVErivACBfd0AdgWA_TXaY",
-    authDomain: "blog-website-73fae.firebaseapp.com",
-    projectId: "blog-website-73fae",
-    storageBucket: "blog-website-73fae.appspot.com",
-    messagingSenderId: "1069380128634",
-    appId: "1:1069380128634:web:f8b6b4c1c16d4dbb70d0c3"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "blog-website-73fae.firebaseapp.com",
+  projectId: "blog-website-73fae",
+  storageBucket: "blog-website-73fae.appspot.com",
+  messagingSenderId: "1069380128634",
+  appId: "1:1069380128634:web:f8b6b4c1c16d4dbb70d0c3"
+};
 
 firebase.initializeApp(firebaseConfig);
 
